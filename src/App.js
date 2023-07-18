9af75559f78af4d61ad12716e8ea7c5de2c5030d
AbdujalilWeb <protoffix1337@gmail.com>
2023-07-18 11:14:12 +0500

    todo end

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { useState } from "react";
+import { Input } from "./Input";
+import { List } from "./List";
 
 function App() {
+  const locals = window.localStorage.getItem("todos")
+  const [state, setState] = useState(locals ? JSON.parse(locals) : [])
   return (
-    <div className="App">
-            
+    <div className="text-light text-center todo__app d-flex align-items-center justify-content-center" style={{minHeight: "100vh"}}>
+      <div className="container">
+        <h1>Todo App</h1>
+        <Input state={state} setState={setState}/>
+        <List setState={setState} state={state}/>
+      </div>
     </div>
   );
 }
